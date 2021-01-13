cb1c39e2b2a9ed392df880a464fb62bc2ca625e5
Luciano Clemente <lucianoaclemente@gmail.com>
2021-01-13 16:02:05 -0300

    Atualização

--- componentes/componente-desafio/src/App.vue
+++ componentes/componente-desafio/src/App.vue
@@ -8,7 +8,17 @@
 </template>
 
 <script>
-export default {
+import Menu from './Menu.vue'
+import Carousel from './Carousel.vue'
+import Toolbar from './Toolbar.vue'
+import Footer from './Footer.vue'
 
+export default {
+  components: {
+    'app-menu': Menu, 
+    'app-carousel': Carousel, 
+    'app-toolbar': Toolbar, 
+    'app-footer': Footer    
+    }
 }
 </script>
--- componentes/componente-desafio/src/main.js
+++ componentes/componente-desafio/src/main.js
@@ -1,17 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import Menu from './Menu.vue'
-import Carousel from './Carousel.vue'
-import Toolbar from './Toolbar.vue'
-import Footer from './Footer.vue'
 
 Vue.config.productionTip = false
-
-Vue.component('app-menu', Menu) 
-Vue.component('app-carousel', Carousel) 
-Vue.component('app-toolbar', Toolbar) 
-Vue.component('app-footer', Footer) 
 
 new Vue({
   vuetify,
