c0bfbecf2b91b749eda4be144f3a1cbe0c17815f
Luciano Clemente <lucianoaclemente@gmail.com>
2021-01-13 15:56:40 -0300

    Atualização

---
+++ componentes/componente-desafio/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <app-menu />
+    <app-toolbar />
+    <app-carousel />
+    <app-footer />
+  </v-app>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ componentes/componente-desafio/src/Menu.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-navigation-drawer app>
+      <v-toolbar flat>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-title class="title">
+                Menu
+            </v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+
+      <v-divider></v-divider>
+
+      <app-menu-item />  
+
+    </v-navigation-drawer>
+</template>
+
+<script>
+
+import MenuItem from './MenuItem.vue'
+
+export default {
+    components : {
+        'app-menu-item':  MenuItem
+    }
+}
+</script>
+
+<style>
+
+</style>
