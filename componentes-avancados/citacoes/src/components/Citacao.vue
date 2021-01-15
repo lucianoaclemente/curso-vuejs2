fcc5370c333f6584b68c0f3ee40a5bab4546ede5
Luciano Clemente <lucianoaclemente@gmail.com>
2021-01-15 10:24:01 -0300

    Atualização

---
+++ componentes-avancados/citacoes/src/components/Citacao.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="citacao">       
+        <!-- slot nomeado -->
+        <slot name="autor"></slot>
+        <!-- slot padrao -->
+        <slot></slot>
+        <!-- slot nomeado -->
+        <slot name="fonte"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+
+    h1 {
+        color: red!important;
+    }
+</style>
