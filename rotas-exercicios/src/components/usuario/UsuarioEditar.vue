c18eeed0bb838122275da09afc16d13ea262c7b8
Luciano Clemente <lucianoaclemente@gmail.com>
2021-01-20 14:33:30 -0300

    Atualização

---
+++ rotas-exercicios/src/components/usuario/UsuarioEditar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="usuario-editar">
+        <h3>Usuário Editar</h3>
+        <hr>
+        <p><strong>Código: </strong>{{ id }}</p>        
+    </div>
+</template>
+
+<script>
+export default {
+    // recuperando por props
+    props: ['id'],
+}
+</script>
+
+<style scoped>
+
+</style>
--- rotas-exercicios/src/router.js
+++ rotas-exercicios/src/router.js
@@ -1,17 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-//import Inicio from './components/Inicio.vue'
-//import Usuario from './components/usuario/Usuario.vue'
+import Inicio from './components/Inicio.vue'
+import Usuario from './components/usuario/Usuario.vue'
+import UsuarioLista from './components/usuario/UsuarioLista.vue'
+import UsuarioDetalhe from './components/usuario/UsuarioDetalhe.vue'
+import UsuarioEditar from './components/usuario/UsuarioEditar.vue'
 
 Vue.use(Router)
 
 export default new Router({
     mode: 'history',
     routes: [{
+        name: 'inicio',
         path: '/',
-        component: require('./components/Inicio.vue').default        
+        component: Inicio
     },{
         path: '/usuario',
-        component: require('./components/usuario/Usuario.vue').default        
+        component: Usuario,
+        props: true,
+        children: [
+            { path: '', component: UsuarioLista, props: true },
+            { path: ':id', component: UsuarioDetalhe, props: true },
+            { path: ':id/editar', component: UsuarioEditar, props: true,
+                name: 'editarUsuario' },
+        ]
+
     }]
 })
