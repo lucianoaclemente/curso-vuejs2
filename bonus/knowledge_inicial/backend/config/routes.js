576ebd7955a8a97fea5ceabe98dc1ab63a43de5c
Luciano Clemente <lucianoaclemente@gmail.com>
2021-02-04 11:22:01 -0300

    Atualização

--- bonus/knowledge_inicial/backend/config/routes.js
+++ bonus/knowledge_inicial/backend/config/routes.js
@@ -11,8 +11,23 @@ module.exports = app => {
         .get( app.api.category.get )
         .post( app.api.category.save )
 
+    app.route('/categories/tree')
+        .get( app.api.category.getTree )
+
     app.route('/categories/:id')
         .get( app.api.category.getById )
         .put( app.api.category.save )
         .delete( app.api.category.remove )
+
+    app.route('/articles')
+        .get( app.api.article.get )
+        .post( app.api.article.save )
+    
+    app.route('/articles/:id')
+        .get( app.api.article.getById )
+        .put( app.api.article.save )
+        .delete( app.api.article.remove )
+    
+    app.route('/categories/:id/articles')
+        .get( app.api.article.getByCategory )
 }
